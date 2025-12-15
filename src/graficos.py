eacda1073e8510a3c31dadc918e18d7c820cdf04
ciencia de datoselias-png <230660920+datascienceelias-png@users.noreply.github.com>
2025-12-15 01:14:33 -0500

    1.1

--- src/graficos.py
+++ src/graficos.py
@@ -1,4 +1,14 @@
-carboidratos: 70%
-grasas: 15 %
-proteina:  %
-    
+import matplotlib.pyplot as plt
+
+def proporcion_macronutrientes(
+    proporcion=[75, 15, 10],
+    macronutrientes=['Carbohidratos', 'Grasas', 'Proteínas']
+   
+):
+    colores = ["#1F74B1", "#F38D30", "#CC243C"]
+    explode = [0, 0, 0.1 ]
+
+    plt.pie(proporcion, labels=macronutrientes, autopct='%1.1f%%', startangle = 360, explode=explode, colors=colores)
+    plt.title('Proporción de Macronutrientes', fontsize=16, fontweight='bold')
+    plt.legend()
+    plt.show()
